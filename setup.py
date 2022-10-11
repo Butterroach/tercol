ca7a5ba200e0535f8ca40b8d6f0af482f01858a8
Butterroach <111113093+Butterroach@users.noreply.github.com>
2022-10-11 18:14:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setup(
     name='tercol',
     version='0.0.4',
     description='TerCol is a simple library that colors your text, I guess',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='',
+    url='https://github.com/Butterroach/tercol/',
     author='Sultan Marzouq',
     author_email='epicnoobcontactemail@gmail.com',
     license='MIT',
     classifiers=classifiers,
     keywords='terminal styling, colors, style',
     packages=find_packages(),
-    install_requires=['']
+    install_requires=[''],
+    download_url = 'https://github.com/Butterroach/tercol/archive/v_004.tar.gz'
 )
